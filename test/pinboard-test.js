1146b199736b1cb8975d602c6f5cb506531c1239
Todd Wolfson <todd@twolfson.com>
2015-01-23 23:30:58 -0600

    Completed test for HTTP and URL

--- test/pinboard-test.js
+++ test/pinboard-test.js
@@ -1,11 +1,49 @@
 // Load in dependencies
 var assert = require('assert');
-var pinboardJs = require('../');
+var Pinboard = require('../');
+
+// Define test utilities
+var pinboardUtils = {
+  init: function (params) {
+    before(function createClient () {
+      this.client = new Pinboard(params);
+    });
+    after(function cleanup () {
+      delete this.client;
+    });
+  },
+  execSync: function (fn) {
+    before(function execFn () {
+      this.result = fn.call(this);
+    });
+    after(function cleanup () {
+      delete this.result;
+    });
+  }
+};
 
 // Start our tests
-describe('pinboard.js', function () {
-  it('returns awesome', function () {
-    assert.strictEqual(pinboardJs(), 'awesome');
+describe('An HTTP pinboard.js user', function () {
+  pinboardUtils.init({
+    auth: {
+      type: 'http',
+      username: 'todd',
+      password: 'password'
+    }
+  });
+
+  describe('building a URL', function () {
+    pinboardUtils.execSync(function buildUrl () {
+      return this.client.buildUrl({
+        pathname: '/posts/get',
+        query: {
+          tag: 'hello'
+        }
+      });
+    });
+
+    it('receives a valid URL', function () {
+      expect(this.result).to.equal('https://todd:password@api.pinboard.in/v1/posts/get?tag=hello');
+    });
   });
 });
-
