ef551280fe594004c56f320c0dcc64bcb8321302
Todd Wolfson <todd@twolfson.com>
2015-01-28 01:31:20 -0600

    Moved to nine-track and scrubbing looks good

--- test/utils/fake-pinboard.js
+++ test/utils/fake-pinboard.js
@@ -1,16 +1,29 @@
 // Load in depdendencies
+var url = require('url');
 var http = require('http');
-var eightTrack = require('eight-track');
+var nineTrack = require('nine-track');
 
-// Define our eight-track server
+// Define our nine-track server
 exports.run = function () {
   var server;
   before(function runFn () {
-    server = http.createServer(eightTrack({
+    server = http.createServer(nineTrack({
       url: 'https://api.pinboard.in',
-      fixtureDir: __dirname + '/../test-files/pinboard-eight-track/',
-      normalizeFn: function (info) {
-        delete info.url;
+      fixtureDir: __dirname + '/../test-files/pinboard-nine-track/',
+      // Normalize/scrub authentication info
+      scrubFn: function (info) {
+        // Parse our URL
+        var req = info.request;
+        var urlObj = url.parse(req.url, true);
+
+        // If there is a query object and we have auth info, normalize it
+        if (urlObj.query && urlObj.query.auth_token) {
+          urlObj.query.auth_token = '*:*';
+          delete urlObj.search;
+
+          // Save our updated URL
+          req.url = url.format(urlObj);
+        }
       }
     }));
     server.listen(9001);
