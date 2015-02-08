add52aad0945128db82ab05581a9aad63a25ed39
Todd Wolfson <todd@twolfson.com>
2015-02-08 02:41:31 -0600

    Added adjusted posts update and test is looking good

--- test/utils/fake-pinboard.js
+++ test/utils/fake-pinboard.js
@@ -26,6 +26,32 @@ var pinboardNineTrack = nineTrack({
   }
 });
 
+// Add per-method proxies
+fakePinboard.addFixture('GET 200 /v1/posts/update', {
+  method: 'get',
+  route: '/v1/posts/update',
+  response: function fakePostsUpdate (localReq, localRes) {
+    // Forward the request
+    pinboardNineTrack.forwardRequest(localReq, function handleResponse (err, remoteRes, remoteBody) {
+      // If there is an error, throw it
+      if (err) {
+        throw err;
+      }
+
+      // Otherwise, adjust the body
+      var remoteJson = JSON.parse(remoteBody);
+      if (remoteJson.update_time) {
+        // Adjust ISO timestamp format (e.g. `2014-01-29T09:42:58Z`)
+        remoteJson.update_time = remoteJson.update_time.replace(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/,
+          '2015-02-01T00:00:00Z');
+      }
+
+      // Send back our response
+      localRes.json(remoteJson);
+    });
+  }
+});
+
 // Add a method to proxy anything
 fakePinboard.addFixture('ALL * *', {
   method: 'all',
