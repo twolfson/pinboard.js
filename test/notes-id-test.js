e0d59455255e7479dd15476bcf2d24666ed47eb3
Todd Wolfson <todd@twolfson.com>
2015-02-13 03:10:30 -0600

    Fixed up assertions for note

--- test/notes-id-test.js
+++ test/notes-id-test.js
@@ -13,7 +13,8 @@ describe('A pinboard.js user looking up a specific note', function () {
   });
   pinboardUtils.execRequest(function buildUrl (done) {
     this.client.notesId({
-      id: 'abc',
+      // DEV: This note had to be created manually
+      id: '96aef597562f7a4f45d1',
       format: 'json'
     }, done);
   });
@@ -21,8 +22,9 @@ describe('A pinboard.js user looking up a specific note', function () {
   it('finds the node', function () {
     expect(this.err).to.equal(null);
     expect(this.res.statusCode).to.equal(200);
-    // {"count":0,"notes":[]}
-    expect(JSON.parse(this.body)).to.have.property('count');
-    expect(JSON.parse(this.body)).to.have.property('notes');
+    // {"id":"96aef597562f7a4f45d1","title","created_at","updated_at","length","text","hash"}
+    expect(JSON.parse(this.body)).to.have.property('id');
+    expect(JSON.parse(this.body)).to.have.property('title');
+    expect(JSON.parse(this.body)).to.have.property('text');
   });
 });
