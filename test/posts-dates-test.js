74ac830183033c5c51a3fe3022ab339de746a2ce
Todd Wolfson <todd@twolfson.com>
2015-02-13 01:19:49 -0600

    Added test for dates endpoint

---
+++ test/posts-dates-test.js
@@ -0,0 +1,27 @@
+// Load in dependencies
+var expect = require('chai').expect;
+var config = require('./utils/config');
+var fakePinboard = require('./utils/fake-pinboard');
+var pinboardUtils = require('./utils/pinboard');
+
+// Start our tests
+describe('A pinboard.js user retrieving dates of posts', function () {
+  fakePinboard.run(['ALL * *']);
+  pinboardUtils.init({
+    auth: config.credentials,
+    url: config.fakePinboardUrl
+  });
+  pinboardUtils.execRequest(function buildUrl (done) {
+    this.client.postsDates({
+      tag: 'nothing',
+      format: 'json'
+    }, done);
+  });
+
+  it('receives a posts', function () {
+    expect(this.err).to.equal(null);
+    expect(this.res.statusCode).to.equal(200);
+    expect(JSON.parse(this.body)).to.have.property('tag', 'nothing');
+    expect(JSON.parse(this.body)).to.have.property('dates');
+  });
+});
